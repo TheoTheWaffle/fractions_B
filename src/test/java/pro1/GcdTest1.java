85d1926dc57afe7149459c1015f91d425e55cc3c
vojta-vorel-uhk <72051011+vojta-vorel-uhk@users.noreply.github.com>
2026-02-19 00:15:53 +0100

    Add files via upload

---
+++ src/test/java/pro1/GcdTest1.java
@@ -0,0 +1,34 @@
+package pro1;
+import org.junit.jupiter.api.Assertions;
+
+class GcdTest1
+{
+    @org.junit.jupiter.api.Test
+    void test()
+    {
+        Assertions.assertEquals(
+                10,
+                NumericUtils.gcd(20,50)
+        );
+
+        Assertions.assertEquals(
+                1,
+                NumericUtils.gcd(1,1)
+        );
+
+        Assertions.assertEquals(
+                10_000_000_000L,
+                NumericUtils.gcd(20_000_000_000L,50_000_000_000L)
+        );
+
+        Assertions.assertEquals(
+                1,
+                NumericUtils.gcd(7919,7907)
+        );
+
+        Assertions.assertEquals(
+                1,
+                NumericUtils.gcd(7907,7919)
+        );
+    }
+}
